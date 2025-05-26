942af14d829175a5b8f0209b2d99a5bf25c9e2c2
maleravi17 <maleravi17@gmail.com>
2025-05-26 13:02:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -194,4 +194,4 @@ async def root():
 # --- Run the App ---
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8002)
